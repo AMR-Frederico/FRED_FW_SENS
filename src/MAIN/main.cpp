cef4de90f3a4db404bb47a618935847ea0b448e4
leticia <leticiarp2000@hotmail.com>
2023-02-19 15:01:24 -0300

    ros_com.h

--- src/MAIN/main.cpp
+++ src/MAIN/main.cpp
@@ -4,56 +4,26 @@
 #include <MAIN/ros_com.h>
 #include <MAIN/imu.h>
 
-#include <ros.h>
-#include <std_msgs/Int32.h>
-#include <std_msgs/Float32.h>
-#include <sensor_msgs/Imu.h>
-
 #include "filter.h"
-//#include <MAIN/laser.h>
-
-#define cmd_led_strip_topic "cmd/led_strip/color"
 
 bool imu_status, dmp_status; 
+bool _connect = false;
 
 float quaternions[4];           //x, y, z, w
 float linaer_acceleration[3];   //x, y, z
 float angular_velocity[3];      //x, y, z 
 
+float yaw = 0; 
+
 int ultrasonic_range[NUMBER_ULTRASONIC_SENSORS];  
 int previousTime; 
 
-//* <------------ ROS setup ---------->
-ros::NodeHandle  nh;
-
 // ----------- ultrasonic sensor - distance
-std_msgs::Int32 leftUltrasonic_distance; 
-std_msgs::Int32 middleUltrasonic_distance; 
-std_msgs::Int32 rightUltrasonic_distance; 
-
-ros::Publisher pubLeftUltrasonic("sensor/ultrasonic/left/distance", &leftUltrasonic_distance); 
-ros::Publisher pubMiddleUltrasonic("sensor/ultrasonic/middle/distance", &middleUltrasonic_distance); 
-ros::Publisher pubRightUltrasonic("sensor/ultrasonic/right/distance", &rightUltrasonic_distance); 
-
-//lights 
-ros::Subscriber<std_msgs::Float32> subLedStrip(cmd_led_strip_topic, led_strip_controler_ros );
-// ---------- imu sensor - orientation
-std_msgs::Float32 imu_yaw; 
-
-ros::Publisher pubIMUyaw("sensor/imu/yaw", &imu_yaw); 
-
-
 
 void setup(){
 
-  nh.initNode(); 
   led_strip_init();
-
-  nh.subscribe(subLedStrip);
-  nh.advertise(pubLeftUltrasonic);
-  nh.advertise(pubMiddleUltrasonic);
-  nh.advertise(pubRightUltrasonic);
-  nh.advertise(pubIMUyaw);
+  ros_init();
 
   pinMode(LED_BUILD_IN,OUTPUT);
   digitalWrite(LED_BUILD_IN,LOW);
@@ -62,23 +32,9 @@ void setup(){
 
   previousTime = millis();
 
-  //nh.getHardware()->setBaud(57600);  
-  
   // check that the IMU initializes correctly
   imu_status, dmp_status = imu_setup();
 
-  // Serial.println(dmp_status); 
-
-  // Serial.println("\n\n\n");
-
-
-  // if (imu_status =!true){
-  //   nh.logwarn("Failed to connect with mpu6050 sensor");
-  // }
-
-  // if (dmp_status =!0){
-  //   nh.logwarn("DMP Initialization failed");
-  // }
 
   if(dmp_status == 0) {
     nh.logwarn("MPU6050 connection successful");
@@ -89,24 +45,20 @@ void setup(){
 
 void loop(){
 
-  int* ultrasonic_range = ultrasonic_measurments(previousTime); 
-  
-  leftUltrasonic_distance.data = ultrasonic_range[0]; 
-  middleUltrasonic_distance.data = ultrasonic_range[1];
-  rightUltrasonic_distance.data = ultrasonic_range[2];
-  
+  if(!rosConnected(nh,_connect)) {
+    pixels.fill(0x000000);
+    pixels.show();
+  }
 
-  pubLeftUltrasonic.publish(&leftUltrasonic_distance);
-  pubMiddleUltrasonic.publish(&middleUltrasonic_distance); 
-  pubRightUltrasonic.publish(&rightUltrasonic_distance);
+  int* ultrasonic_range = ultrasonic_measurments(previousTime); 
 
   //! only get the sensor data if the IMU started correctly
   if (dmp_status ==0 ){
 
-    imu_yaw.data = imu_get_yaw(); 
-    pubIMUyaw.publish(&imu_yaw); 
+    yaw = imu_get_yaw(); 
   }
-
+  ros_loop(ultrasonic_range[0], ultrasonic_range[1], 
+           ultrasonic_range[2], yaw);
 
   nh.spinOnce();
 
--- src/MAIN/ros_com.h
+++ src/MAIN/ros_com.h
@@ -1,8 +1,41 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
+#include <sensor_msgs/Imu.h>
+
 #include <Arduino.h>
 #include <MAIN/config.h>
+
+// -----> PUB --------------------------------------   
+#define left_ultrasonic_topic "sensor/ultrasonic/left/distance"
+#define middle_ultrasonic_topic "sensor/ultrasonic/middle/distance"
+#define right_ultrasonic_topic "sensor/ultrasonic/right/distance"
+
+#define imu_yaw_topic "sensor/imu/yaw"
+
+// -----> SUBS --------------------------------------
+#define cmd_led_strip_topic "cmd/led_strip/color"
+
+
+// ----- PUB MSG ------------------------------------
+std_msgs::Int32 leftUltrasonicMsg;
+ros::Publisher pubLeftUltrasonic(left_ultrasonic_topic, &leftUltrasonicMsg); 
+
+std_msgs::Int32 middleUltrasonicMsg; 
+ros::Publisher pubMiddleUltrasonic(middle_ultrasonic_topic, &middleUltrasonicMsg); 
+
+std_msgs::Int32 rightUltrasonicMsg; 
+ros::Publisher pubRightUltrasonic(right_ultrasonic_topic, &rightUltrasonicMsg); 
+
+std_msgs::Float32 imuYawMsg; 
+ros::Publisher pubIMUyaw(imu_yaw_topic, &imuYawMsg); 
+
+// ----- SUBS MSG ----------------------------------
+ros::Subscriber<std_msgs::Float32> subLedStrip(cmd_led_strip_topic, led_strip_controler_ros );
+
+//* <------------ ROS setup ---------->
+ros::NodeHandle  nh;
 
 bool rosConnected(ros::NodeHandle  nh,bool _connect){
     bool connected = nh.connected();
@@ -11,4 +44,33 @@ bool rosConnected(ros::NodeHandle  nh,bool _connect){
         digitalWrite(LED_BUILD_IN,!connected);
     }
     return connected;
+}
+
+void ros_init(){
+
+    nh.initNode(); 
+
+    nh.subscribe(subLedStrip); 
+
+    nh.advertise(pubLeftUltrasonic); 
+    nh.advertise(pubMiddleUltrasonic); 
+    nh.advertise(pubRightUltrasonic);
+    nh.advertise(pubIMUyaw); 
+
+}
+
+void ros_loop( int left_ultrasonic,   int middle_ultrasonic,
+               int right_ultrasonic,  int imu_yaw) {
+    
+    leftUltrasonicMsg.data = left_ultrasonic; 
+    pubLeftUltrasonic.publish(&leftUltrasonicMsg); 
+
+    middleUltrasonicMsg.data = middle_ultrasonic; 
+    pubMiddleUltrasonic.publish(&middleUltrasonicMsg); 
+
+    rightUltrasonicMsg.data = right_ultrasonic; 
+    pubRightUltrasonic.publish(&rightUltrasonicMsg); 
+
+    imuYawMsg.data = imu_yaw; 
+    pubIMUyaw.publish(&imuYawMsg);
 }
