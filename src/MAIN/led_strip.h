e1f369f792d5be3e0c626bd7bfbb38f54d46d1ec
Maria Francisca <maria@qualicode.com.br>
2023-02-19 10:48:47 -0300

    Mais cores adicionadas

--- src/MAIN/led_strip.h
+++ src/MAIN/led_strip.h
@@ -15,7 +15,8 @@ int long green = 0x00FF00 ;
 int long magenta = 0xFF00FF ;
 int long yellow = 0xFFFF00 ;
 int long orange = 0xFFA500 ;
-
+int long purple = 0x6B238E  ;
+int long white = 0xFFFFFF ;
 
 void led_strip_controler_ros(const std_msgs::Float32& msg){
     float color = msg.data;
